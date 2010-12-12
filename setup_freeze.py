9574a1e0dafd4841e3c77f13483d0f0f58691dbc
deoren <deoren@users.noreply.github.com>
2010-12-12 00:49:22 +0000

    [issues #10,#43] * Removed the FIXME tag from setup_freeze.py as those were warnings, not necessarily errors. The modules mentioned are not (known to be) required for Synclosure. * Added the VERSION_PLACEHOLDER tag so build.py can replace it with build info. * Muted rows of debug/info output (warnings show) * Added author and url values.
    git-svn-id: http://projects.whyaskwhy.org/svn/synclosure/trunk@45 26afa1be-71c7-4335-a220-9b9f906cb6c8

--- setup_freeze.py
+++ setup_freeze.py
@@ -5,21 +5,15 @@ from cx_Freeze import setup, Executable
 
 setup(
     name = "Synclosure",
-    version = "0.2",
+    version = "VERSION_PLACEHOLDER",
+    author='TBD',
+    url='http://projects.whyaskwhy.org/projects/synclosure/',
     description = "Synclosure is a RSS aggregator to flexibly download " \
         "files in enclosures. It supports filter keywords, custom actions " \
         "and a caching mechanism.",
-    executables = [Executable("synclosure.py")])
+    options = {'build_exe':{"silent": True}},
+    executables = [Executable("synclosure.py")]
+    )
 
 # To compile, do the following:
 # python setup_freeze.py build
-
-
-# FIXME: The following error was given when trying to compile
-
-# Missing modules:
-# ? chardet imported from feedparser
-# ? cjkcodecs.aliases imported from feedparser
-# ? iconv_codec imported from feedparser
-# ? mx.Tidy imported from feedparser
-# ? tidy imported from feedparser
