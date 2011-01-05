e64eb0b793ca8f795e4d41830dedf3f753fff444
deoren <deoren@users.noreply.github.com>
2011-01-05 16:19:45 +0000

    [issues #74] Fixed #74 - Adding icon to synclosure.exe during build.
    git-svn-id: http://projects.whyaskwhy.org/svn/synclosure/trunk@121 26afa1be-71c7-4335-a220-9b9f906cb6c8

--- setup_freeze.py
+++ setup_freeze.py
@@ -2,6 +2,7 @@
 # $HeadURL$
 
 from cx_Freeze import setup, Executable
+import os
 
 release_version = "VERSION_PLACEHOLDER"
 
@@ -10,16 +11,29 @@ if release_version[0:3] == "dev":
 else:
     exe_version = release_version
 
+Exe_Target = Executable (
+      # what to build
+      script = "synclosure.py",
+      initScript = None,
+      targetName = "synclosure.exe",
+      compress = True,
+      copyDependentFiles = True,
+      appendScriptToExe = False,
+      appendScriptToLibrary = False,
+      icon = 'installer' + os.sep + 'synclosure.ico',
+      #copyright = 'Original code and name (C) by raphael balimann, 2004. Modifications (C) 2007 WhyAskWhy.org',
+    )
+    
 setup(
     name = "Synclosure",
     version = exe_version,
-    author='TBD',
+    author='WhyAskWhy.org',
     url='http://projects.whyaskwhy.org/projects/synclosure/',
     description = "Synclosure is a RSS aggregator to flexibly download " \
         "files in enclosures. It supports filter keywords, custom actions " \
         "and a caching mechanism.",
     options = {'build_exe':{"silent": True}},
-    executables = [Executable("synclosure.py")]
+    executables = [Exe_Target]
     )
 
 # To compile, do the following:
